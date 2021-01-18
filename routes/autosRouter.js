3c083aad7d718ee05204793100aa79b149d23e4b
EricFormar <66625345+EricFormar@users.noreply.github.com>
2021-01-18 12:17:27 -0300

    iniciando partials...

--- routes/autosRouter.js
+++ routes/autosRouter.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const autosController = require('../controllers/autosController');
+
+router.get('/',autosController.index);
 
 
 
