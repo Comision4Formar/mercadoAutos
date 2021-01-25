99fdd71542f81f00bff56ef369b5ff5f8130837f
EricFormar <66625345+EricFormar@users.noreply.github.com>
2021-01-25 13:57:14 -0300

    CRUD terminado

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -26,7 +26,7 @@ module.exports = {
         const {marca,modelo,color,anio,img} = req.body;
 
         const auto = {
-            id: lastID + 1,
+            id: Number(lastID + 1),
             marca,
             modelo,
             color,
@@ -42,11 +42,38 @@ module.exports = {
     },
     carsEdit : (req,res) => {
         
+        const auto = autos.find(auto => auto.id === +req.params.id);
+
+        res.render('admin/carsEdit',{
+            auto
+        })
     },
     carsUpdate : (req,res) => {
-        
+        const {marca,modelo,color,anio,img} = req.body;
+
+        autos.forEach(auto => {
+            if(auto.id === +req.params.id){
+                auto.id = Number(req.params.id);
+                auto.marca = marca;
+                auto.modelo = modelo;
+                auto.anio = anio;
+                auto.color = color;
+                auto.img = img
+            }
+        });
+
+        fs.writeFileSync('./data/autos.json',JSON.stringify(autos),'utf-8');
+        res.redirect('/admin/autos/list');
     },
     carsDelete : (req,res) => {
+        autos.forEach(auto => {
+            if(auto.id === +req.params.id){
+                var aEliminar = autos.indexOf(auto);
+                autos.splice(aEliminar,1)
+            }
+        });
         
+        fs.writeFileSync('./data/autos.json',JSON.stringify(autos),'utf-8');
+        res.redirect('/admin/autos/list');
     }
 }
